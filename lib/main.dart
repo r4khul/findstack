3b7491bb895f57e01596c7611de9e623d04c6e13
r4khul <blackempire9177@gmail.com>
2026-01-02 15:34:37 +0530

    init: project init and clean up

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FindStackApp());
+}
+
+class FindStackApp extends StatelessWidget {
+  const FindStackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
